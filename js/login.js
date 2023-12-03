84be364942df1f59e91a8c2b7811450e8a6d0e01
Artem Potapov <110378317+Artem-Pot@users.noreply.github.com>
2023-12-03 18:32:57 +0400

    Промежуточный вариант формы регистрации

--- js/login.js
+++ js/login.js
@@ -10,14 +10,12 @@ login.addEventListener('input', function() {
         login.style.border = '2px solid red';
         error.style.display = 'block';
         error.textContent = 'Недопустимый символ';
-        event.preventDefault(); 
     }
 
-    if (login.value.length > 30) {
+    else if (login.value.length > 30) {
         login.style.border = '2px solid red';
         error.style.display = 'block';
         error.textContent = 'Длина больше 30 символов';
-        event.preventDefault(); 
     }
 
     else {
@@ -26,7 +24,7 @@ login.addEventListener('input', function() {
     }
 })
 
-//проверка поля воода после нажатия кнопки
+//проверка поля ввода после нажатия кнопки
 function loginСheck() {
     if (login.value === '') {
         login.style.border = '2px solid red';
@@ -35,21 +33,21 @@ function loginСheck() {
         event.preventDefault(); 
     }
 
-    if (login.value !== login.value.trim().replace(/[^а-яА-Яa-zA-Z0-9-_]/g, "")){ // проверка что в логине нет недопустимых символов
+    else if (login.value !== login.value.trim().replace(/[^а-яА-Яa-zA-Z0-9-_]/g, "")){ // проверка что в логине нет недопустимых символов
         login.style.border = '2px solid red';
         error.style.display = 'block';
         error.textContent = 'Недопустимый символ';
         event.preventDefault(); 
     }
 
-    if (login.value.length < 6) {
+    else if (login.value.length < 6) {
         login.style.border = '2px solid red';
         error.style.display = 'block';
         error.textContent = 'Длина меньше 6 символов';
         event.preventDefault(); 
     }
 
-    if (login.value.length > 30) {
+    else if (login.value.length > 30) {
         login.style.border = '2px solid red';
         error.style.display = 'block';
         error.textContent = 'Длина больше 30 символов';
@@ -59,29 +57,20 @@ function loginСheck() {
 
 //проверка поля ввода пароля в реальном времени
 password.addEventListener('input', function() {
-    if (password.value === '') {
-        password.style.border = '2px solid red';
-        error.style.display = 'block';
-        error.textContent = 'Поле не должно быть пустым';
-        event.preventDefault(); 
-    }
-
     if (password.value.length > 30) {
         password.style.border = '2px solid red';
         error.style.display = 'block';
         error.textContent = 'Длина больше 30 символов';
-        event.preventDefault(); 
     }
 
     else {
         password.style.border = '';
         error.style.display = 'none';
-        login.value.trim(); // удаление лишних пробелов спереди и сзади
     }
 })
 
-//проверка поля ввода пароля после нажития кнопки
-function loginPassword() {
+//проверка поля ввода пароля после нажатия кнопки
+function passwordСheck() {
     if (password.value === '') {
         password.style.border = '2px solid red';
         error.style.display = 'block';
@@ -103,24 +92,15 @@ function loginPassword() {
         event.preventDefault(); 
     }
 
-    else {
-        login.value.trim(); // удаление лишних пробелов спереди и сзади
-    }
+    password.value.trim(); // удаление лишних пробелов спереди и сзади
 }
 
 buttonLogin.onclick = function() {
     loginСheck();
-    loginPassword();
-
+    passwordСheck();
 }
 
-//сделать больше подсказок
-//Как минимум одна заглавная и одна строчная буква.
-//Допускается наличие следующих символов: ~ ! ? @ # $ % ^ & * _ - + ( ) [ ] { } > < / \ | " ' . , :
-//пароль не должен быть равен логину
-//Пароль должен содержать не менее 8 символов. 
-//писать что пробелы не должны быть в начале
+
+//добавить фукнцию просмотра пароля при вводе
 //добавить капчу после 5 неправильных введений
 //сделать отправку потом через пост
-//сдлеать второе поля для дублирования пароля 
-//хеширование пароля с помощью md5, SHA-1/SHA-256
---
+++ js/register.js
@@ -0,0 +1,219 @@
+const error = document.querySelector('.error');
+const login = document.querySelector('#login');
+const password = document.querySelector('#password');
+const passwordRepeat = document.querySelector('#password_2');
+const checkbox = document.querySelector('#checkbox');
+const buttonLogin = document.querySelector('.button_login');
+
+//Проверка ввода логина
+//проверка поля ввода логина в реальном времени
+login.addEventListener('input', function() {
+    if (login.value !== login.value.trim().replace(/[^а-яА-Яa-zA-Z0-9-_]/g, "")){ // проверка что в логине нет недопустимых символов
+        login.style.border = '2px solid red';
+        error.style.display = 'block';
+        error.textContent = 'Недопустимый символ';
+        console.log(`Недопустимый символ логина`);
+    }
+
+    else if (login.value.length > 30) {
+        login.style.border = '2px solid red';
+        error.style.display = 'block';
+        error.textContent = 'Длина больше 30 символов';
+        console.log(`Длина больше 30 символов логина`);
+    }
+
+    else {
+        login.style.border = '';
+        error.style.display = 'none';
+    }
+})
+
+//проверка поля логина после нажатия кнопки
+function loginСheck() {
+    if (login.value === '') {
+        login.style.border = '2px solid red';
+        error.style.display = 'block';
+        error.textContent = 'Поле не должно быть пустым';
+        event.preventDefault(); 
+        console.log(`Поле логина не должно быть пустым`);
+    }
+
+    else if (login.value !== login.value.trim().replace(/[^а-яА-Яa-zA-Z0-9-_]/g, "")){ // проверка что в логине нет недопустимых символов
+        login.style.border = '2px solid red';
+        error.style.display = 'block';
+        error.textContent = 'Недопустимый символ';
+        console.log(`Недопустимый символ логина`);
+        event.preventDefault(); 
+    }
+
+    else if (login.value.length < 6) {
+        login.style.border = '2px solid red';
+        error.style.display = 'block';
+        error.textContent = 'Длина меньше 6 символов';
+        console.log(`Длина меньше 6 символов логина`);
+        event.preventDefault(); 
+    }
+
+    else if (login.value.length > 30) {
+        login.style.border = '2px solid red';
+        error.style.display = 'block';
+        error.textContent = 'Длина больше 30 символов';
+        console.log(`Длина больше 30 символов логина`);
+        event.preventDefault(); 
+    }
+}
+
+//Проверка ввода пароля
+//проверка поля ввода пароля в реальном времени
+password.addEventListener('input', function() {
+    if (password.value.length > 30) {
+        password.style.border = '2px solid red';
+        error.style.display = 'block';
+        error.textContent = 'Длина больше 30 символов';
+        console.log(`Длина больше 30 символов пароля`);
+    }
+
+    else {
+        password.style.border = '';
+        error.style.display = 'none';
+    }
+})
+
+//проверка поля ввода пароля после нажатия кнопки
+function passwordСheck() {
+    if (password.value === '') {
+        password.style.border = '2px solid red';
+        error.style.display = 'block';
+        error.textContent = 'Поле не должно быть пустым';
+        event.preventDefault(); 
+        console.log(`Пароль не должен быть пустым`);
+    }
+
+    else if (password.value.length < 6) {
+        password.style.border = '2px solid red';
+        error.style.display = 'block';
+        error.textContent = 'Длина меньше 6 символов';
+        event.preventDefault(); 
+        console.log(`Длина пароля меньше 6 символов`);
+    }
+
+    else if (password.value.length > 30) {
+        password.style.border = '2px solid red';
+        error.style.display = 'block';
+        error.textContent = 'Длина больше 30 символов';
+        event.preventDefault();
+        console.log(`Длина пароля больше 30 символов`); 
+    }
+
+    else if (password.value === password.value.toLowerCase()) {
+        password.style.border = '2px solid red';
+        error.style.display = 'block';
+        error.textContent = 'Пароль должен содержать минимум одну заглавную букву';
+        event.preventDefault(); 
+        console.log(`Пароль должен содержать минимум одну заглавную букву`);
+    }
+
+    else if (password.value === password.value.toUpperCase()) {
+        password.style.border = '2px solid red';
+        error.style.display = 'block';
+        error.textContent = 'Пароль должен содержать минимум одну строчную букву';
+        event.preventDefault(); 
+        console.log(`Пароль должен содержать минимум одну строчную букву`);
+    }
+
+    else {
+        password.value.trim(); // удаление лишних пробелов спереди и сзади
+    }
+}
+
+// Повтор ввода пароля
+// проверка поля повтора пароля в реальном времени
+passwordRepeat.addEventListener('input', function() {
+    if (passwordRepeat.value.length > 30) {
+        passwordRepeat.style.border = '2px solid red';
+        error.style.display = 'block';
+        error.textContent = 'Длина больше 30 символов';
+        console.log(`Длина больше 30 символов повтора пароля`);
+    }
+
+    else {
+        passwordRepeat.style.border = '';
+        error.style.display = 'none';
+    }
+})
+
+//проверка поля повтора пароля после нажатия кнопки
+function passwordСheckRepeat() {
+    if (passwordRepeat.value === '') {
+        passwordRepeat.style.border = '2px solid red';
+        error.style.display = 'block';
+        error.textContent = 'Поле не должно быть пустым';
+        console.log(`Поле не должно быть пустым повтора пароля`);
+        event.preventDefault(); 
+    }
+
+    if (passwordRepeat.value.length < 6) {
+        passwordRepeat.style.border = '2px solid red';
+        error.style.display = 'block';
+        error.textContent = 'Длина меньше 6 символов';
+        console.log(`Длина меньше 6 символов повтора пароля`);
+        event.preventDefault(); 
+    }
+
+    if (passwordRepeat.value.length > 30) {
+        passwordRepeat.style.border = '2px solid red';
+        error.style.display = 'block';
+        error.textContent = 'Длина больше 30 символов';
+        console.log(`Длина больше 30 символов повтора пароля`);
+        event.preventDefault(); 
+    }
+
+    passwordRepeat.value.trim(); // удаление лишних пробелов спереди и сзади
+}
+
+//функция сравнения пароля и повтора пароля
+function comparePasswords() {
+    if (password.value !== passwordRepeat.value) {
+        passwordRepeat.style.border = '2px solid red';
+        error.style.display = 'block';
+        error.textContent = 'Пароли не совпадают';
+        console.log(`Пароли не совпадают`);
+        event.preventDefault(); 
+    }
+    else {
+        passwordRepeat.style.border = '';
+        password.style.border = '';
+        error.style.display = 'none';
+    }
+}
+
+//функция проверки не совпадения пароля и логина
+function comparisonLoginPassword (){
+    if (login.value === password.value) {
+        login.style.border = '2px solid red';
+        password.style.border = '2px solid red';
+        error.style.display = 'block';
+        error.textContent = 'Логин и пароль не должны совпадать';
+        console.log(`Логин и пароль не должны совпадать`);
+        event.preventDefault(); 
+    }
+    else {
+        login.style.border = '';
+        password.style.border = '';
+        error.style.display = 'none';
+    }
+}
+
+//проверка всех полей ввода после нажатия на кнопку
+buttonLogin.onclick = function() {
+    loginСheck();
+    passwordСheck();
+    passwordСheckRepeat();
+    comparePasswords();
+    comparisonLoginPassword ();
+}
+
+//Как минимум одна цифра для пароля
+//сделать функцию показа пароля или его скрытие
+//хеширование пароля с помощью md5, SHA-1/SHA-256
+//сделать отправку потом через пост
