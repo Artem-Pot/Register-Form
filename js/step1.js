933f8ec385f129d5421d5b03875f24337d91bfd4
Artem <blbo@yandex.ru>
2023-12-11 16:26:22 +0400

    Закончен шаг 2

--- js/step1.js
+++ js/step1.js
@@ -23,11 +23,9 @@ function errorPattern(entryField, errorInput, errorText) {
 surname.addEventListener('input', function() {
     if (surname.value !== surname.value.replace(/[^а-яА-Яa-zA-ZЁё]/g, '')){
         errorPattern(surname, errorSurname,`Недопустимый символ `);
-        console.log(`Недопустимый символ фамилии`);
     }
     else if (surname.value.length > 100) {
         errorPattern(surname, errorSurname,'Длина больше 100 символов');
-        console.log(`Длина больше 30 символов фамилии`);
     }
     else {
         surname.style.border = '';
@@ -40,17 +38,14 @@ function surnameСheck() {
     if (surname.value === '') {
         errorPattern(surname, errorSurname, 'Поле не должно быть пустым');
         event.preventDefault(); 
-        console.log(`Поле фамилии не должно быть пустым`);
     }
     else if (surname.value !== surname.value.replace(/[^а-яА-Яa-zA-ZЁё]/g, "")){
         errorPattern(surname, errorSurname, 'Недопустимый символ');
         event.preventDefault(); 
-        console.log(`Недопустимый символ фамилии`);
     }
     else if (surname.value.length > 100) {
         errorPattern(surname, errorSurname, 'Длина больше 100 символов');
         event.preventDefault(); 
-        console.log(`Длина больше 30 символов фамилии`);
     }
     return surname.value.trim();
 }
@@ -59,11 +54,9 @@ function surnameСheck() {
 name.addEventListener('input', function() {
     if (name.value !== name.value.replace(/[^а-яА-Яa-zA-ZЁё]/g, '')){
         errorPattern(name, errorName,`Недопустимый символ `);
-        console.log(`Недопустимый символ имени`);
     }
     else if (name.value.length > 100) {
         errorPattern(name, errorName,'Длина больше 100 символов');
-        console.log(`Длина больше 100 символов имени`);
     }
     else {
         name.style.border = '';
@@ -76,17 +69,14 @@ function nameСheck() {
     if (name.value === '') {
         errorPattern(name, errorName, 'Поле не должно быть пустым');
         event.preventDefault(); 
-        console.log(`Поле имени не должно быть пустым`);
     }
     else if (name.value !== name.value.replace(/[^а-яА-Яa-zA-ZЁё]/g, "")){
         errorPattern(name, errorName, 'Недопустимый символ');
         event.preventDefault(); 
-        console.log(`Недопустимый символ имени`);
     }
     else if (name.value.length > 100) {
         errorPattern(name, errorName, 'Длина больше 100 символов');
         event.preventDefault(); 
-        console.log(`Длина больше 100 символов имени`);
     }
     return name.value.trim();
 }
@@ -96,11 +86,9 @@ function nameСheck() {
 patronymic.addEventListener('input', function() {
     if (patronymic.value !== patronymic.value.replace(/[^а-яА-Яa-zA-ZЁё]/g, '')){
         errorPattern(patronymic, errorPatronymic,`Недопустимый символ `);
-        console.log(`Недопустимый символ отчества`);
     }
     else if (patronymic.value.length > 100) {
         errorPattern(patronymic, errorPatronymic,'Длина больше 100 символов');
-        console.log(`Длина больше 100 символов отчества`);
     }
     else {
         patronymic.style.border = '';
@@ -113,12 +101,10 @@ function patronymicСheck() {
     if (patronymic.value !== patronymic.value.replace(/[^а-яА-Яa-zA-ZЁё]/g, "")){
         errorPattern(patronymic, errorPatronymic, 'Недопустимый символ');
         event.preventDefault(); 
-        console.log(`Недопустимый символ отчества`);
     }
     else if (patronymic.value.length > 100) {
         errorPattern(patronymic, errorPatronymic, 'Длина больше 100 символов');
         event.preventDefault(); 
-        console.log(`Длина больше 30 символов отчества`);
     }
     return patronymic.value.trim();
 }
@@ -128,14 +114,11 @@ function patronymicСheck() {
 email.addEventListener('input', function() {
     if (email.value.length > 100) {
         errorPattern(passwordRepeat, errorEmail, 'Длина больше 100 символов');
-        console.log(`Длина больше 100 символов емайла`);
     }
     else if (email.value !== email.value.replace(/[^a-zA-Z0-9-_.@]/g, "")){ // проверка что в логине нет недопустимых символов
         errorPattern(email, errorEmail, 'Недопустимый символ');
         event.preventDefault(); 
-        console.log(`Недопустимый символ емайла`);
     }
-
     else {
         email.style.border = '';
         errorEmail.style.display = 'none';
@@ -147,41 +130,33 @@ function emailСheck() {
     if (email.value === '') {
         errorPattern(email, errorEmail, 'Поле не должно быть пустым');
         event.preventDefault(); 
-        console.log(`Поле не должно быть пустым емайла`);
     }
     else if (email.value.length < 5) {
         errorPattern(email, errorEmail, 'Длина меньше 5 символов');
         event.preventDefault(); 
-        console.log(`Длина меньше 5 символов емайла`);
     }
     else if (email.value.length > 100) {
         errorPattern(email, errorEmail, 'Длина больше 100 символов');
         event.preventDefault(); 
-        console.log(`Длина больше 100 символов емайла`);
     }
     else if (email.value !== email.value.replace(/[^a-zA-Z0-9-_.@]/g, "")){ 
         errorPattern(email, errorEmail, 'Недопустимый символ');
         event.preventDefault(); 
-        console.log(`Недопустимый символ емайла`);
     }
     else if (email.value !== email.value.replace(/^[^a-zA-Z0-9][a-zA-Z0-9-_]+@[a-zA-Z0-9-]+.[^a-zA-Z0-9]/g, "")) {
         errorPattern(email, errorEmail, 'Некорректный Email');
         event.preventDefault(); 
-        console.log(`Некорректный Email `);
     }
     return email.value.trim().toLowerCase();
 }
-
 //----------------------------------------------------------------
 //проверка поля ввода телефона
 telephone.addEventListener('input', function() {
     if (telephone.value !== telephone.value.replace(/[^0-9+]/g, '')){ // проверка что в логине нет недопустимых символов
         errorPattern(telephone, errorTelephone,`Недопустимый символ `);
-        console.log(`Недопустимый символ телефона`);
     }
     else if (telephone.value.length > 12) {
         errorPattern(telephone, errorTelephone,'Длина больше 12 символов');
-        console.log(`Длина больше 12 символов телефона`);
     }
     else {
         telephone.style.border = '';
@@ -194,31 +169,23 @@ function telephoneСheck() {
     if (telephone.value === '') {
         errorPattern(telephone, errorTelephone, 'Поле не должно быть пустым');
         event.preventDefault(); 
-        console.log(`Поле телефона не должно быть пустым`);
     }
     else if (telephone.value !== telephone.value.trim().replace(/[^0-9+]/g, "")){ // проверка что в логине нет недопустимых символов
         errorPattern(telephone, errorTelephone, 'Недопустимый символ');
         event.preventDefault(); 
-        console.log(`Недопустимый символ телефона`);
     }
     else if (telephone.value.length < 5) {
         errorPattern(telephone, errorTelephone,'Длина меньше 5 символов');
         event.preventDefault(); 
-        console.log(`Длина меньше 5 символов телефона`);
     }
-
     else if (telephone.value.length > 12) {
         errorPattern(telephone, errorTelephone, 'Длина больше 12 символов');
         event.preventDefault(); 
-        console.log(`Длина больше 12 символов телефона`);
     }
-
     else if (telephone.value !== telephone.value.replace(/[^+]+[^0-9]/g, "")) {
         errorPattern(telephone, errorTelephone, 'Некорректный телефон');
         event.preventDefault(); 
-        console.log(`Некорректный телефон`);
     }
-
     return telephone.value.trim().toLowerCase();
 }
 
@@ -230,13 +197,9 @@ buttonNext.onclick = function() {
     telephoneСheck();
 }
 
-//добавить остальные поля (город проживания, текстовое поле..,  чек - пол, политика соглашения)
-
-
 // разбить на модули и каждый скрипт в отдельный файл
 //добавить сообщение об удачной отправки и перемещения на страницу авторизации
 //добавить сервер для принятия данных и авторизации
 //хеширование пароля с помощью md5, SHA-1/SHA-256
 //сделать отправку потом через пост
-//добавить какой именно символ недопустим в логине и пароле
 
--- js/step2.js
+++ js/step2.js
@@ -1,24 +1,151 @@
 const errorCountry = document.querySelector('.error_country');
 const errorCity = document.querySelector('.error_city');
-const errorDate = document.querySelector('.error_date'); 
+const errorDate = document.querySelector('.error_date');
+const errorComment = document.querySelector('.error_comment');
 
 const country = document.querySelector('#country');
 const city = document.querySelector('#city'); 
 const date = document.querySelector('#date'); 
 const comment = document.querySelector('#comment'); 
 
+const male = document.querySelector('#male'); 
+const female = document.querySelector('#female'); 
+
 const buttonNext = document.querySelector('.button_next');
 
+//шаблон вывода ошибки
+function errorPattern(entryField, errorInput, errorText) {
+    entryField.style.border = '2px solid red';
+    errorInput.style.display = 'block';
+    errorInput.textContent = errorText;
+}
 
+//--------------------------------------------------------
+//проверка поля ввода страны в реальном времени
+country.addEventListener('input', function() {
+    if (country.value !== country.value.replace(/[^а-яА-Яa-zA-ZЁё-\s]/g, '')){
+        errorPattern(country, errorCountry,`Недопустимый символ`);
+    }
+    else if (country.value.length > 60) {
+        errorPattern(country, errorCountry,'Длина больше 60 символов');
+    }
+    else {
+        country.style.border = '';
+        errorCountry.style.display = 'none';
+    }
+})
 
+//проверка поля страны после нажатия на кнопку next
+function countryСheck() {
+    if (country.value === '') {
+        errorPattern(country, errorCountry, 'Поле не должно быть пустым');
+        event.preventDefault(); 
+    }
+    else if (country.value !== country.value.replace(/[^а-яА-Яa-zA-ZЁё-\s]/g, "")){
+        errorPattern(country, errorCountry, 'Недопустимый символ');
+        event.preventDefault(); 
+    }
+    else if (country.value.length > 60) {
+        errorPattern(country, errorCountry, 'Длина больше 60 символов');
+        event.preventDefault(); 
+    }
+    else if (country.value.length < 3) {
+        errorPattern(country, errorCountry,'Длина меньше 3 символов');
+    }
+}
 
+//--------------------------------------------------------
+//проверка поля ввода города в реальном времени
+city.addEventListener('input', function() {
+    if (city.value !== city.value.replace(/[^а-яА-Яa-zA-ZЁё-\s]/g, '')){
+        errorPattern(city, errorCity,`Недопустимый символ`);
+    }
+    else if (city.value.length > 60) {
+        errorPattern(city, errorCity,'Длина больше 60 символов');
+    }
+    else {
+        city.style.border = '';
+        errorCity.style.display = 'none';
+    }
+})
 
+//проверка поля города после нажатия на кнопку next
+function cityСheck() {
+    if (city.value === '') {
+        errorPattern(city, errorCity, 'Поле не должно быть пустым');
+        event.preventDefault(); 
+    }
+    else if (city.value !== city.value.replace(/[^а-яА-Яa-zA-ZЁё-\s]/g, '')){
+        errorPattern(city, errorCity, 'Недопустимый символ');
+        event.preventDefault(); 
+    }
+    else if (city.value.length > 60) {
+        errorPattern(city, errorCity, 'Длина больше 60 символов');
+        event.preventDefault(); 
+    }
+    else if (city.value.length < 3) {
+        errorPattern(city, errorCity,'Длина меньше 3 символов');
+    }
+}
+//----------------------------------------------------------------
+//проверка поля с датой
+function dateCheck(){
+    if (date.value === '') {
+        errorPattern(date, errorDate, 'Укажите дату');
+        event.preventDefault(); 
+    }
+    else {
+        date.style.border = '';
+        errorDate.style.display = 'none';
+    }
+}
+
+//проверка радио кнопок
+function radiCheck(){
+    if(male.checked) {
+        console.log('Выбрана кнопка: мужской пол');
+    }
+    if(female.checked) {
+        console.log('Выбрана кнопка: женский пол');
+    } 
+}
+
+//----------------------------------------------------------------
+//Проверка поля комментария в реальном времени
+comment.addEventListener('input', function() {
+    if (comment.value !== comment.value.replace(/[^а-яА-Яa-zA-ZЁё0-9\s@"№#$;%:^&?*()/|,.'<>`~+-_\{}]/g, '')){
+        errorPattern(comment, errorComment,`Недопустимый символ`);
+    }
+    else if (comment.value.length > 500) {
+        errorPattern(comment, errorComment,'Длина больше 500 символов');
+    }
+    else {
+        comment.style.border = '';
+        errorComment.style.display = 'none';
+    }
+})
+
+//проверка поля города после нажатия на кнопку next
+function commentСheck() {
+    if (comment.textContent !== comment.textContent.replace(/[^а-яА-Яa-zA-ZЁё0-9\s!@"№#$;%:^&?*()/|,.'<>`~+-_[]\{}]/g, '')){
+        errorPattern(comment, errorComment, 'Недопустимый символ');
+        event.preventDefault(); 
+    }
+    else if (comment.textContent.length > 500) {
+        errorPattern(comment, errorComment, 'Длина больше 500 символов');
+        event.preventDefault(); 
+    }
+    
+}
 
 buttonNext.onclick = function() {
     countryСheck();
     cityСheck();
+    dateCheck();
+    radiCheck();
     commentСheck();
 }
 
-
+//Добавить проверку чтобы город и страна не начинались и не кончались "-"
 //добавить проверку радио кнопок
+//что делать дальше с
